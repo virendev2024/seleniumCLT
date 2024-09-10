3e9ee47be42817efe55e254cc09a40ee85429558
virender singh <virensingh2022@gmail.com>
2024-09-10 15:22:38 +0100

    added headless browser configurations

--- src/test/java/Tests/ChromeTest.java
+++ src/test/java/Tests/ChromeTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -11,7 +12,13 @@ public class ChromeTest {
     @Test
     public void testChromeDriver() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        System.setProperty("webdriver.chrome.verboseLogging", "true");
+        options.addArguments("window-size=1920,1200");
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
         WebElement searchBox = driver.findElement(By.xpath("//textarea[@class=\"gLFyf\"]")); // also fixed the xpath
         searchBox.sendKeys("Selenium WebDriver");
--- src/test/java/Tests/EdgeTest.java
+++ src/test/java/Tests/EdgeTest.java
@@ -5,13 +5,20 @@ import org.openqa.selenium.WebDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.annotations.Test;
 
 public class EdgeTest {
     @Test
     public void testEdgeDriver() throws InterruptedException {
         WebDriverManager.edgedriver().setup();
-        WebDriver driver = new EdgeDriver();
+        EdgeOptions options=new EdgeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        System.setProperty("webdriver.chrome.verboseLogging", "true");
+        options.addArguments("window-size=1920,1200");
+        WebDriver driver = new EdgeDriver(options);
         driver.get("https://www.google.com");
         WebElement searchBox = driver.findElement(By.xpath("//textarea[@class=\"gLFyf\"]"));
         searchBox.sendKeys("Selenium WebDriver");
